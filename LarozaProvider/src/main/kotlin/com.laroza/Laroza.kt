1c9fc956f4ed26366ec2761f731e6708ab6f82cb
alyabroudy1 <alyabroudy1@gmail.com>
2026-02-20 12:25:37 +0100

    feat(Laroza): Implement generic DrmPlayerDialog for Shahid DRM links

--- LarozaProvider/src/main/kotlin/com.laroza/Laroza.kt
+++ LarozaProvider/src/main/kotlin/com.laroza/Laroza.kt
@@ -14,6 +14,7 @@ import com.lagradost.cloudstream3.utils.loadExtractor
 import com.lagradost.cloudstream3.utils.Qualities
 import com.lagradost.cloudstream3.utils.ExtractorLinkType
 import com.lagradost.cloudstream3.utils.newExtractorLink
+import com.cloudstream.shared.ui.DrmPlayerDialog
 import kotlinx.coroutines.*
 import kotlinx.coroutines.sync.Mutex
 import kotlinx.coroutines.sync.withLock
@@ -36,5 +37,38 @@ class Laroza : BaseProvider() {
 
     override fun getParser(): NewBaseParser {
         return LarozaParser()
+    }
+
+    override suspend fun loadLinks(
+        data: String,
+        isCasting: Boolean,
+        subtitleCallback: (SubtitleFile) -> Unit,
+        callback: (ExtractorLink) -> Unit
+    ): Boolean {
+        // Intercept the callback from BaseProvider
+        var handledByDialog = false
+        val interceptingCallback: (ExtractorLink) -> Unit = { link ->
+            // If it's a Shahid stream (which uses DRM), launch DrmPlayerDialog
+            if (link.url.contains("shahid.net") && link.url.contains(".mpd")) {
+                Log.d("Laroza", "Intercepted DRM Shahid link, launching DrmPlayerDialog")
+                handledByDialog = true
+                ActivityProvider.currentActivity?.let { activity ->
+                    activity.runOnUiThread {
+                        // Pass the referer URL (embed page) to the dialog to load the DRM player
+                        val dialogUrl = link.referer ?: link.url
+                        val dialog = DrmPlayerDialog(activity, dialogUrl, link.referer)
+                        dialog.show()
+                    }
+                }
+            } else {
+                // Normal link, pass to ExoPlayer
+                callback(link)
+            }
+        }
+        
+        val success = super.loadLinks(data, isCasting, subtitleCallback, interceptingCallback)
+        
+        // If we handled it with the dialog, we consider it a success even if no ExoPlayer links were returned
+        return success || handledByDialog
     }
 }
