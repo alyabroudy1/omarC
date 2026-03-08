93b44d5bb7519aabae0e60f1c67750ffc88972c8
alyabroudy1 <alyabroudy1@gmail.com>
2026-03-08 18:22:43 +0100

    Disable YouTube provider for existing users

--- YoutubeProvider/build.gradle.kts
+++ YoutubeProvider/build.gradle.kts
@@ -4,7 +4,7 @@ cloudstream {
     authors = listOf("omarflex")
     language = "en"
     description = "Standalone YouTube Provider"
-    status = 3
+    status = 0
     tvTypes = listOf("Movie", "TvSeries")
     iconUrl = "https://raw.githubusercontent.com/recloudstream/cloudstream/master/app/src/main/ic_launcher-playstore.png"
 }
