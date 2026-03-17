c6551ef53e77422053ac016c75d2c5b5541d29ed
mohammad <mohammad@blueoak.io>
2026-03-17 16:19:36 +0100

    fix: Initialize PluginContext to prevent ClassCastException

--- AkwamProvider/src/main/kotlin/com.akwam/AkwamPlugin.kt
+++ AkwamProvider/src/main/kotlin/com.akwam/AkwamPlugin.kt
@@ -3,10 +3,12 @@ package com.akwam
 import android.content.Context
 import com.lagradost.cloudstream3.plugins.CloudstreamPlugin
 import com.lagradost.cloudstream3.plugins.Plugin
+import com.cloudstream.shared.android.PluginContext
 
 @CloudstreamPlugin
 class AkwamPlugin : Plugin() {
     override fun load(context: Context) {
+        PluginContext.init(context)
         registerMainAPI(Akwam())
     }
 }
