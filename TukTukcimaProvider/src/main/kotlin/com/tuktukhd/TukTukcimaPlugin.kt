4c288c2b07429ce7ade022b9e565eb34c2124681
mohammad <mohammad@blueoak.io>
2026-03-18 11:50:36 +0100

    TukTukcimaPlugin.kt

--- TukTukcimaProvider/src/main/kotlin/com/tuktukhd/TukTukcimaPlugin.kt
+++ TukTukcimaProvider/src/main/kotlin/com/tuktukhd/TukTukcimaPlugin.kt
@@ -3,10 +3,12 @@ package com.tuktukhd
 import com.lagradost.cloudstream3.plugins.CloudstreamPlugin
 import com.lagradost.cloudstream3.plugins.Plugin
 import android.content.Context
+import com.cloudstream.shared.android.PluginContext
 
 @CloudstreamPlugin
 class TukTukcimaPlugin : Plugin() {
     override fun load(context: Context) {
+        PluginContext.init(context)
         registerMainAPI(TukTukcima())
     }
 }
