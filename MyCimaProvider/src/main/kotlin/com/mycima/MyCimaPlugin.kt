f4f8b0ca9bf5d0f6e33747461914d793a614da17
mohammad <mohammad@blueoak.io>
2026-03-19 12:49:03 +0100

    mycima

--- MyCimaProvider/src/main/kotlin/com/mycima/MyCimaPlugin.kt
+++ MyCimaProvider/src/main/kotlin/com/mycima/MyCimaPlugin.kt
@@ -4,10 +4,12 @@ import com.lagradost.cloudstream3.plugins.CloudstreamPlugin
 import com.lagradost.cloudstream3.plugins.Plugin
 import android.content.Context
 import com.cloudstream.shared.extractors.registerSharedExtractors
+import com.cloudstream.shared.android.PluginContext
 
 @CloudstreamPlugin
 class MyCimaPlugin : Plugin() {
     override fun load(context: Context) {
+        PluginContext.init(context)
         registerMainAPI(MyCima())
         registerSharedExtractors()
     }
