4dd252fd378e2c0a2483c12223a4ebc825e0ac52
mohammad <mohammad@blueoak.io>
2026-07-10 20:32:57 +0200

    cimanow headless 2

--- SyriaLiveProvider/src/main/kotlin/com/syrialive/SyriaLive.kt
+++ SyriaLiveProvider/src/main/kotlin/com/syrialive/SyriaLive.kt
@@ -14,6 +14,7 @@ import android.util.Base64
 
 class SyriaLive : BaseProvider() {
 
+    override var name = "سيريا لايف (كورة)"
     override val baseDomain get() = "d.syrlive.com/"
     override val providerName get() = "SyriaLive"
     override var lang = "ar"
